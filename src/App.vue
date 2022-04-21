d5bba45d5545fd826394dd4e353fd89c4fa0b509
Vitor <costec222@gmail.com>
2022-04-21 20:16:10 -0300

    praticando tipagem no Vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="class">
+    
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent, reactive, ref, toRefs } from 'vue';
+import Job from './types/Job'
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {},
+  setup() {
+    const jobs = ref<Job[]>([
+      { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1'},
+      { title: 'Vendor', location: 'Canada', salary: 48000, id: '2'},
+      { title: 'Student', location: 'California', salary: 5000, id: '3'},
+      { title: 'Programmer', location: 'Ohio', salary: 170000, id: '4'},
+      { title: 'Engineering', location: 'Nevada', salary: 95000, id: '5'},
+      { title: 'Teacher', location: 'New York', salary: 150000, id: '6'},
+    ])
+    return { jobs }
+  },
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
